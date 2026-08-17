97203d1fe2eea80fe50e580f8ddf213b13a6ea40
Narutoooo_Dev <flashstore801@gmail.com>
2026-08-18 00:07:38 +0300

    feat: Add founder group

--- app/src/main/java/org/thoughtcrime/securesms/groups/GroupAccessControl.java
+++ app/src/main/java/org/thoughtcrime/securesms/groups/GroupAccessControl.java
@@ -9,6 +9,7 @@ import org.thoughtcrime.securesms.database.GroupTable;
 public enum GroupAccessControl {
   ALL_MEMBERS(R.string.GroupManagement_access_level_all_members),
   ONLY_ADMINS(R.string.GroupManagement_access_level_only_admins),
+  ONLY_FOUNDER(R.string.GroupManagement_access_level_only_founder),
   NO_ONE(R.string.GroupManagement_access_level_no_one);
 
   private final @StringRes int string;
@@ -28,6 +29,7 @@ public enum GroupAccessControl {
     return switch (this) {
       case ALL_MEMBERS -> memberLevel.isInGroup();
       case ONLY_ADMINS -> memberLevel == GroupTable.MemberLevel.ADMINISTRATOR;
+      case ONLY_FOUNDER -> memberLevel == GroupTable.MemberLevel.FOUNDER;
       case NO_ONE -> false;
     };
   }
